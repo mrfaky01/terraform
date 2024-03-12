4ec52055d984bdcc2034e84b86d851cdf7241d81
Mrfaky01 <farukyahaya572@gmail.com>
2024-03-11 23:58:01 -0500

    updated ami

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Replace with the AMI ID you wish to use
+  ami                    = "ami-0f403e3180720dd7e"  # Replace with the AMI ID you wish to use
   instance_type          = "t2.micro"
   key_name               = "my-keypair"             # Replace with your key pair name
   subnet_id              = aws_subnet.my_subnet.id
