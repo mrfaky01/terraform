df466d8fda8f4ca7731442c289ad901cb7c96863
mrfaky01 <157874785+mrfaky01@users.noreply.github.com>
2024-03-11 23:50:30 -0500

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0c55b159cbfafe1f0"  # Replace with the AMI ID you wish to use
+  instance_type          = "t2.micro"
+  key_name               = "my-keypair"             # Replace with your key pair name
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
